bc0c970318154a5fa470122925dee1144fa9c296
AaronVillegas5 <akvilleg@uci.edu>
2026-04-04 16:50:25 -0700

    Refactor asthma prediction logic and add cardiovascular model inputs

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List, Optional
 import pandas as pd
+import joblib
 from Asthma.asthma_statistics import add_percentile_rankings
 from Asthma.asthma_model import load_model, load_and_clean_data, predict_full_dataset
 
@@ -14,12 +15,34 @@ app = FastAPI(title="Asthma Risk API", version="1.0")
 XGB_MODEL_PATH = "Asthma/best_xgb_model.pkl"
 DATA_PATH = "Asthma/data.csv"
 
+#ASTHMA--------------------------------------------------------------------------------
 df_clean, X, y = load_and_clean_data(DATA_PATH)
-xgb_model = load_model(XGB_MODEL_PATH)
+asthma_model = load_model(XGB_MODEL_PATH)
 
 # Add predictions and percentiles for all ZIPs
-df_clean["pred_asthma_pctl"] = xgb_model.predict(X)
+df_clean["pred_asthma_pctl"] = asthma_model.predict(X)
 df_clean = add_percentile_rankings(df_clean, pred_col="pred_asthma_pctl")
+
+
+#Cardiovascular--------------------------------------------------------------------------------
+FEATURES = [
+    "Ozone", "PM2.5", "Diesel PM", "Drinking Water", "Lead", "Pesticides", "Traffic", "Cleanup Sites", "Groundwater Threats", "Haz Waste", "Imp Water Bodies", "Solid Waste", "Education", "Linguistic Isolation", "Poverty", "Unemployment", "Housing Burden"
+]
+
+def load_cardio_model(path="Cardio/cardiovascular_model.pkl"):
+    return joblib.load(path)
+
+def load_cardio_data(csv_path):
+    df = pd.read_csv(csv_path)
+    df = df.dropna(subset=FEATURES)
+    X = df[FEATURES]
+    return df, X
+
+def predict_cardio(df, X, model):
+    df = df.copy()
+    df["pred_cardio"] = model.predict(X)
+    return df
+
 
 # ---------------------------
 # Request / Response Models
@@ -42,13 +65,13 @@ class ZIPResponse(BaseModel):
 def read_root():
     return {"message": "Welcome to the Healthy Home API!"}
 
-@app.get("/top-risk")
+@app.get("/top-risk-asthma")
 def get_top_risk(percentile: float = 90):
     top_df = df_clean[df_clean["pred_asthma_pctl"] >= percentile].copy()
     return top_df[["ZIP", "pred_asthma_pctl", "state_percentile", "county_percentile",
                    "top_5_state", "top_10_county"]].to_dict(orient="records")
 
-@app.post("/predict", response_model=List[ZIPResponse])
+@app.post("/predict-asthma", response_model=List[ZIPResponse])
 def predict_zip(request: ZIPRequest):
     zips = request.zip_codes
     filtered = df_clean[df_clean["ZIP"].isin(zips)].copy()
